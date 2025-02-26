1f002d9bc8e7da00cf6388a1a67053c16ba09c95
antonshk2023 <antonshk2013@gmail.com>
2025-02-26 14:42:58 +0100

    linux 9

---
+++ data basess/home_task_9.sql
@@ -0,0 +1,61 @@
+/*Вывести текущую дату и время.*/
+
+SELECT DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s') as curent_date;
+
+/*Вывести текущий год и месяц*/
+
+SELECT DATE_FORMAT(NOW(), '%Y-%m') as curent_year_and_month;
+
+/*Вывести текущее время*/
+
+SELECT DATE_FORMAT(NOW(), '%H:%i:%s') as curent_year_and_month;
+
+/*Вывести название текущего дня недели*/
+
+SELECT
+CASE
+WHEN WEEKDAY(NOW()) = 0 THEN 'Воскресение'
+WHEN WEEKDAY(NOW()) = 1 THEN 'Понедельник'
+WHEN WEEKDAY(NOW()) = 2 THEN 'Вторник'
+WHEN WEEKDAY(NOW()) = 3 THEN 'Среда'
+WHEN WEEKDAY(NOW()) = 4 THEN 'Четверг'
+WHEN WEEKDAY(NOW()) = 5 THEN 'Пятница'
+WHEN WEEKDAY(NOW()) = 6 THEN 'Суббота'
+END as 'week_day';
+
+
+/*Вывести номер текущего месяца*/
+
+SELECT DATE_FORMAT(NOW(), '%m') as curent_month;
+
+/*Вывести номер дня в дате “2020-03-18”*/
+
+SELECT DATE_FORMAT('2020-03-18', '%d') as curent_day;
+
+/*Подключиться к базе данных shop (которая находится на удаленном сервере).*/
+use shop;
+/*Определить какие из покупок были совершены апреле (4-й месяц)*/
+SELECT
+*
+FROM ORDERS o
+where DATE_FORMAT(o.ODATE , '%m')='04';
+/*Определить количество покупок в 2022-м году*/
+SELECT
+*
+FROM ORDERS o
+where DATE_FORMAT(o.ODATE , '%Y')='2022';
+/*Определить, сколько покупок было совершено в
+каждый день. Отсортировать строки в порядке возрастания
+количества покупок. Вывести два поля - дату и количество покупок*/
+SELECT
+o.ODATE
+,COUNT(o.ORDER_ID) as count_orders
+FROM ORDERS o
+group by o.ODATE
+order by count_orders
+
+/*Определить среднюю стоимость покупок в апреле*/
+SELECT
+round(avg(o.AMT), 2)
+from ORDERS o
+where DATE_FORMAT(o.ODATE , '%m')='04';
